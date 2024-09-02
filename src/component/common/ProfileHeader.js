a920afb12992a08c3cbb0a760894a7eeb6991da5
aayan398 <maayan200510@gmail.com>
2024-09-02 10:40:54 +0100

    profile header1

--- src/component/common/ProfileHeader.js
+++ src/component/common/ProfileHeader.js
@@ -1,72 +1,137 @@
-import {React, useState }from 'react'
-import { Link, useLocation, useNavigate } from "react-router-dom";
-import { Row, Upload, Col, message } from 'antd';
+import React, { useState } from 'react';
+import { Upload, message, Divider, Space } from 'antd';
 import { FiUpload } from "react-icons/fi";
+import Column from 'antd/es/table/Column';
 
 function ProfileHeader() {
     const [imageUrl, setImageUrl] = useState("");
-    const location = useLocation();
     const [loading, setLoading] = useState(false);
+
     const customRequest = ({ file, onSuccess, onError }) => {
         setTimeout(() => {
-          if (file) {
-            onSuccess({ url: URL.createObjectURL(file) }, file);
-          } else {
-            onError(new Error("Upload failed."));
-          }
+            if (file) {
+                onSuccess({ url: URL.createObjectURL(file) }, file);
+            } else {
+                onError(new Error("Upload failed."));
+            }
         }, 1000);
-      };
-      const handleChange1 = (info) => {
+    };
+
+    const handleChange1 = (info) => {
         if (info.file.status === "uploading") {
-          setLoading(true);
-          return;
+            setLoading(true);
+            return;
         }
         if (info.file.status === "done") {
-          setLoading(false);
-          setImageUrl(info.file.response.url);
-          message.success("Image uploaded successfully");
+            setLoading(false);
+            setImageUrl(info.file.response.url);
+            message.success("Image uploaded successfully");
         } else if (info.file.status === "error") {
-          setLoading(false);
-          message.error("Image upload failed.");
+            setLoading(false);
+            message.error("Image upload failed.");
         }
-      };
-  return (
-    <div className='profil-container-main d-flex flex-column'>
+    };
 
-            
-              
-                  <Upload
+    return (
+        <div className='profil-container-main d-flex flex-column'>
+            <div className="profile-header-container" style={{ display: 'flex', alignItems: 'center' }}>
+                <Upload
                     customRequest={customRequest}
                     showUploadList={false}
                     onChange={handleChange1}
                     accept="image/*"
-                  >
+                >
                     <div className="d-flex flex-column">
-                      {imageUrl ? (
-                        <img
-                          src={imageUrl}
-                          alt="Uploaded"
-                          style={{
-                            width: "150px",
-                            height: "auto",
-                          }}
-                        />
-                      ) : (
-                        <div className="profile-image">
-                          <FiUpload className="upload-icon" />
-
-                          <h1>JS</h1>
-                        </div>
-                      )}
+                        {imageUrl ? (
+                            <img
+                                src={imageUrl}
+                                alt="Uploaded"
+                                style={{
+                                    width: "100px",
+                                    height: "100px",
+                                    borderRadius: "50%",
+                                    color: "#FF0000",
+                                }}
+                            />
+                        ) : (
+                            <div className="profile-image" style={{
+                                width: "100px",
+                                height: "100px",
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                borderRadius: "50%",
+                                backgroundColor: "#FF0000",
+                                fontSize: "32px",
+                                color: "#FF0000",
+                            }}>
+                                <FiUpload className="upload-icon" />
+                                <h1 style={{ margin: 0 }}>JS</h1>
+                            </div>
+                        )}
                     </div>
-                  </Upload>
-                
+                </Upload>
+                <Divider type='vertical' style={{ height: "100%", margin: "0 20px", backgroundColor: "#FFFFFF" }} />
+                <div style={{ display: 'flex', flexDirection: 'column' }}>
+                    <div style={{ fontWeight: 'bold', color: '#FFD700', fontSize: '12px' }}>Code 56606L</div>
+                    <div style={{ fontWeight: 'bold', color: '#6B7AAB', fontSize: '12px' }}>Jack Smith</div>
+                    <div style={{ fontWeight: 'bold', color: '#6B7AAB', fontSize: '12px' }}>20/06/1979 (36 Y)</div>
+                    <div style={{ fontWeight: 'bold', color: '#6B7AAB', fontSize: '12px' }}>Married</div>
+                    <div style={{ fontWeight: 'bold', color: '#FF7F7F', fontSize: '12px' }}>11/10/2015 (Deceased)</div>
+                </div>
+            </div>
+            <Divider type='horizontal' />
+            
+            <div style={{ display: 'flex', flexDirection: 'column', fontWeight: 'bold', color: '#555', fontSize: '12px', color: '#6B7AAB' }}>
+                   <div>Cases:</div>
+                   <br/>
+                   <div>Claims:</div>
+                   <br/>
+                   <div>Member (01/01/2020 - 11/10/2015)</div>
+                   <div>Draduated on: 01/09/2007</div>
+                   <div>Attested on: 01/01/2008</div>
+                   <br/>
+                   <div>Statue</div>
+                   <br/>
+                   <div>0001 Garda</div>
+                   <div>0021 Garda</div>
+                   <div>0109 CDU-SDU</div>
+                   <div>0026 CDU/SDU</div>
+                <Divider type='horizontal' />
+                   <div>District Rep:</div>
+                   <br/>
+                   <div>District Secretary:</div>
+                   <br/>
+                   <div>District Chairman:</div>
+                   <br/>
+                   <div>CEC Rep:</div>
+                <Divider type='horizontal' />
+                   <div>Life Assurance:</div>
+                <br/>
+                <div style={{ display: 'flex', justifyContent: 'flex-start', fontWeight: 'bold', color: '#555', fontSize: '12px', color: '#6B7AAB' }}>
+                   <span style={{ marginRight: '45px' }}>Member</span>
+                   <span>Partner</span>
+                </div>
+                   <br/>
+                   <div>Special Illness:</div>
+                   <br/>
+                <div style={{ display: 'flex', justifyContent: 'flex-start', fontWeight: 'bold', color: '#555', fontSize: '12px' , color: '#6B7AAB'}}>
+                <br/>
+                  <span style={{ marginRight: '45px' }}>Member</span>
+                  <span>Partner</span>
+                </div>
+                <div style={{ display: 'flex', flexDirection: 'column', fontWeight: 'bold', color: '#555', fontSize: '12px', color: '#6B7AAB' }}>
+                  <br/>
+                <div>Illness & Injury</div>
+                <br/>
+                <div>Legal Assistance</div>
+                <br/>
+                <div>Salary Protection</div>
+                </div>
+            </div>
 
-           
-         
-          <h1 className="primary-contact">Profile Header</h1>
-    </div>
-  )
+        </div>
+    )
 }
 
-export default ProfileHeader
+export default ProfileHeader;
---
+++ src/component/common/TableComponent.js
@@ -0,0 +1,145 @@
+import React, { useEffect,useRef } from "react";
+import { Table, Pagination,Space } from "antd";
+import { useTableColumns } from "../../context/TableColumnsContext ";
+import Gridmenu from "./Gridmenu";
+import { usePDF } from 'react-to-pdf';
+import { MoreOutlined } from "@ant-design/icons";
+import { maleTblData } from "../../Data";
+import { PiSlidersHorizontalBold } from "react-icons/pi";
+import { LuRefreshCw } from "react-icons/lu";
+import { Link } from "react-router-dom";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { CgAttachment } from "react-icons/cg";
+import SimpleMenu from "./SimpleMenu";
+import { tableData } from "../../Data";
+
+function TableComponent({ dataSource }) {
+  const inputRef = useRef(null);
+  const { toPDF, targetRef } = usePDF({filename: 'page.pdf'});
+  const testing = {
+    Delete: "false",
+    Attached:"false",
+    view:'false'
+  };
+  const { columns, state,isMale,gridData } = useTableColumns();
+  const nameColumnIndex = columns.findIndex((col) => col.key === "Name");
+  const modifiedColumns = columns.map((col, index) => {
+    if (index === nameColumnIndex) {
+      return {
+        ...col,
+        render: (text, record) => (
+          <Link
+            to={`/Details`}
+            style={{ color: "blue" }}
+            state={{ search: "Profile", name: record.Name }}
+          >
+            {text}
+          </Link>
+        ),
+      };
+    }
+    return col;
+  });
+
+  const actionColumn = {
+    title: () => (
+      <Gridmenu
+        title={
+          <PiSlidersHorizontalBold
+            style={{ fontSize: "24px", color: "white", fontWeight: 600 }}
+          />
+        }
+        data={{
+          RegNo: true,
+          Name: true,
+          Rank: false,
+          Duty: false,
+          Station: false,
+          Distric: false,
+          Division: false,
+          Address: true,
+          Status: false,
+          Updated: false,
+          alpha: false,
+          beta: false,
+          giga: false,
+        }}
+      />
+    ),
+    key: "actions",
+    width: 70,
+    render: (_, record) => (
+      <Space size="small" className="action-buttons">
+        <CgAttachment />
+        <SimpleMenu
+          title={
+            <>
+              {" "}
+              <BsThreeDotsVertical />
+            </>
+          }
+          data={testing}
+          checkbox={false}
+          isSearched={false}
+          isTransparent={true}
+        />
+      </Space>
+    ),
+  };
+console.log(gridData,"gridData")
+
+
+  return (
+    <div className="common-table" ref={inputRef}>
+      <Table 
+        columns={[actionColumn, ...modifiedColumns]}
+        dataSource={gridData}
+        scroll={{ x: "100%", y: 300 }}
+        pagination={false}
+        footer={() => (
+          <div className="d-flex justify-content-between">
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                width: "100%",
+              }}
+            >
+              <span
+                style={{
+                  marginRight: "4px",
+                  fontSize: "12px",
+                  fontWeight: "500",
+                }}
+              >
+                1-{gridData.length}
+              </span>
+              <span
+                style={{
+                  marginRight: "4px",
+                  fontSize: "12px",
+                  fontWeight: "500",
+                }}
+              >
+                {" "}
+                of {`${gridData.length}`}
+              </span>
+              <LuRefreshCw />
+            </div>
+            <Pagination
+              defaultCurrent={1}
+              total={gridData.length}
+              pageSize={10}
+            />
+          </div>
+        )}
+        rowClassName={(record, index) =>
+          index % 2 !== 0 ? "odd-row" : "even-row"
+        }
+      />
+    </div>
+  );
+}
+
+export default TableComponent;
