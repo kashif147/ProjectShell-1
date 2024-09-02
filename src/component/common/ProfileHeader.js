92bc796cf031d11953040d6b27df38bd1fe1d29a
aayan398 <maayan200510@gmail.com>
2024-09-02 10:59:10 +0100

    profile header changed

--- src/component/common/ProfileHeader.js
+++ src/component/common/ProfileHeader.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
-import { Upload, message, Divider, Space } from 'antd';
+import { Upload, message, Divider } from 'antd';
 import { FiUpload } from "react-icons/fi";
-import Column from 'antd/es/table/Column';
 
 function ProfileHeader() {
     const [imageUrl, setImageUrl] = useState("");
@@ -34,7 +33,11 @@ function ProfileHeader() {
 
     return (
         <div className='profil-container-main d-flex flex-column'>
-            <div className="profile-header-container" style={{ display: 'flex', alignItems: 'center' }}>
+            <div className="profile-header-container" style={{ 
+                display: 'flex', 
+                justifyContent: 'center',  // Center horizontally
+                alignItems: 'center',       // Center vertically
+            }}>
                 <Upload
                     customRequest={customRequest}
                     showUploadList={false}
@@ -50,7 +53,6 @@ function ProfileHeader() {
                                     width: "100px",
                                     height: "100px",
                                     borderRadius: "50%",
-                                    color: "#FF0000",
                                 }}
                             />
                         ) : (
@@ -63,7 +65,7 @@ function ProfileHeader() {
                                 borderRadius: "50%",
                                 backgroundColor: "#FF0000",
                                 fontSize: "32px",
-                                color: "#FF0000",
+                                color: "#FF0000", // Change the color to white for better visibility
                             }}>
                                 <FiUpload className="upload-icon" />
                                 <h1 style={{ margin: 0 }}>JS</h1>
@@ -71,7 +73,11 @@ function ProfileHeader() {
                         )}
                     </div>
                 </Upload>
-                <Divider type='vertical' style={{ height: "100%", margin: "0 20px", backgroundColor: "#FFFFFF" }} />
+            </div>
+            <Divider type='horizontal' />
+            
+            {/* Other content remains unchanged */}
+            <div style={{ display: 'flex', flexDirection: 'column', fontWeight: 'bold', fontSize: '12px', color: '#6B7AAB' }}>
                 <div style={{ display: 'flex', flexDirection: 'column' }}>
                     <div style={{ fontWeight: 'bold', color: '#FFD700', fontSize: '12px' }}>Code 56606L</div>
                     <div style={{ fontWeight: 'bold', color: '#6B7AAB', fontSize: '12px' }}>Jack Smith</div>
@@ -79,57 +85,52 @@ function ProfileHeader() {
                     <div style={{ fontWeight: 'bold', color: '#6B7AAB', fontSize: '12px' }}>Married</div>
                     <div style={{ fontWeight: 'bold', color: '#FF7F7F', fontSize: '12px' }}>11/10/2015 (Deceased)</div>
                 </div>
-            </div>
-            <Divider type='horizontal' />
-            
-            <div style={{ display: 'flex', flexDirection: 'column', fontWeight: 'bold', color: '#555', fontSize: '12px', color: '#6B7AAB' }}>
-                   <div>Cases:</div>
-                   <br/>
-                   <div>Claims:</div>
-                   <br/>
-                   <div>Member (01/01/2020 - 11/10/2015)</div>
-                   <div>Draduated on: 01/09/2007</div>
-                   <div>Attested on: 01/01/2008</div>
-                   <br/>
-                   <div>Statue</div>
-                   <br/>
-                   <div>0001 Garda</div>
-                   <div>0021 Garda</div>
-                   <div>0109 CDU-SDU</div>
-                   <div>0026 CDU/SDU</div>
-                <Divider type='horizontal' />
-                   <div>District Rep:</div>
-                   <br/>
-                   <div>District Secretary:</div>
-                   <br/>
-                   <div>District Chairman:</div>
-                   <br/>
-                   <div>CEC Rep:</div>
-                <Divider type='horizontal' />
-                   <div>Life Assurance:</div>
                 <br/>
-                <div style={{ display: 'flex', justifyContent: 'flex-start', fontWeight: 'bold', color: '#555', fontSize: '12px', color: '#6B7AAB' }}>
+                <div>Cases:</div>
+                <br/>
+                <div>Claims:</div>
+                <br/>
+                <div>Member (01/01/2020 - 11/10/2015)</div>
+                <div>Graduated on: 01/09/2007</div>
+                <div>Attested on: 01/01/2008</div>
+                <br/>
+                <div>Statue</div>
+                <br/>
+                <div>0001 Garda</div>
+                <div>0021 Garda</div>
+                <div>0109 CDU-SDU</div>
+                <div>0026 CDU/SDU</div>
+                <Divider type='horizontal' />
+                <div>District Rep:</div>
+                <br/>
+                <div>District Secretary:</div>
+                <br/>
+                <div>District Chairman:</div>
+                <br/>
+                <div>CEC Rep:</div>
+                <Divider type='horizontal' />
+                <div>Life Assurance:</div>
+                <br/>
+                <div style={{ display: 'flex', justifyContent: 'flex-start', fontWeight: 'bold', fontSize: '12px' }}>
                    <span style={{ marginRight: '45px' }}>Member</span>
                    <span>Partner</span>
                 </div>
-                   <br/>
-                   <div>Special Illness:</div>
-                   <br/>
-                <div style={{ display: 'flex', justifyContent: 'flex-start', fontWeight: 'bold', color: '#555', fontSize: '12px' , color: '#6B7AAB'}}>
                 <br/>
-                  <span style={{ marginRight: '45px' }}>Member</span>
-                  <span>Partner</span>
+                <div>Special Illness:</div>
+                <br/>
+                <div style={{ display: 'flex', justifyContent: 'flex-start', fontWeight: 'bold', fontSize: '12px' }}>
+                    <span style={{ marginRight: '45px' }}>Member</span>
+                    <span>Partner</span>
                 </div>
-                <div style={{ display: 'flex', flexDirection: 'column', fontWeight: 'bold', color: '#555', fontSize: '12px', color: '#6B7AAB' }}>
-                  <br/>
-                <div>Illness & Injury</div>
-                <br/>
-                <div>Legal Assistance</div>
-                <br/>
-                <div>Salary Protection</div>
+                <div style={{ display: 'flex', flexDirection: 'column', fontWeight: 'bold', fontSize: '12px' }}>
+                    <br/>
+                    <div>Illness & Injury</div>
+                    <br/>
+                    <div>Legal Assistance</div>
+                    <br/>
+                    <div>Salary Protection</div>
                 </div>
             </div>
-
         </div>
     )
 }
