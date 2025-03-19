1111b174915e4007c9129a10b287c7ab5491b954
Kashif <kashif147@gmail.com>
2025-03-19 16:04:36 +0000

    review code

--- src/component/msft/email/ComposeEmailButton.js
+++ src/component/msft/email/ComposeEmailButton.js
@@ -11,4 +11,12 @@ function ComposeEmailButton() {
 
   export default ComposeEmailButton
 
-  
+  /**
+   * This component renders a button that when clicked will open a new compose window for Outlook.
+   * The window is opened in a new tab.
+   * The URL for the compose window is https://outlook.office.com/mail/deeplink/compose.
+   * This URL is an undocumented Microsoft endpoint for opening a new compose window with the Outlook web app.
+   * The URL does not require any authentication or authorization to access.
+   * The button is rendered with the text "Compose New Email".
+   * The button is given an onClick event handler that opens the compose window when clicked.
+   */
--- src/component/msft/email/OpenOutlookButton.js
+++ src/component/msft/email/OpenOutlookButton.js
@@ -1,6 +1,16 @@
 import React from 'react';
 
+/**
+ * A simple React component that renders a button.
+ * When clicked, the button opens the Outlook web app in a new tab.
+ * The URL used for the link is 'https://outlook.office.com/mail/'.
+ * This is a published URL by Microsoft for accessing Outlook web.
+ * @returns {ReactElement} A button with an "Open Outlook Web" label.
+ */
 const OpenOutlookButton = () => {
+  /**
+   * Opens the Outlook web app in a new tab.
+   */
   const openOutlookWeb = () => {
     window.open('https://outlook.office.com/mail/', '_blank'); // Opens Outlook in a new tab
   };
