ac98da7002491e757aa511a561c332c1bd785583
FazalAzim <fazalazim238@gmail.com>
2025-04-12 19:17:52 +0500

    feature/Configuation of toast

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import './styles/Utilites.css'
 import Entry from './Entry';
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { TableColumnsProvider } from './context/TableColumnsContext ';
 import { Provider } from 'react-redux'
 import store from './store';
 import { NotificationProvider } from './context/NotificationContext';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './config/globals.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,6 +17,7 @@ root.render(
     <Router>
       <NotificationProvider>
         <TableColumnsProvider>
+          <ToastContainer />
           <App />
         </TableColumnsProvider>
       </NotificationProvider>
@@ -25,4 +28,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
