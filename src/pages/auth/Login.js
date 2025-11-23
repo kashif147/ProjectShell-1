eacc5ed156310fb9ba13ef9ef57f2ea233ab3d07
Kashif <kashif147@gmail.com>
2025-11-23 22:03:05 +0000

    Update redirect URI in Login component to use environment variable or current origin

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -84,7 +84,7 @@ const Login = () => {
     localStorage.setItem("pkce_code_verifier", codeVerifier);
     const tenantId = "39866a06-30bc-4a89-80c6-9dd9357dd453";
     const clientId = "ad25f823-e2d3-43e2-bea5-a9e6c9b0dbae";
-    const redirectUri = "http://localhost:3000";
+    const redirectUri = process.env.REACT_APP_REDIRECT_URI || window.location.origin;
     const scopes = "openid profile email offline_access";
     const authUrl = new URL(
       `https://login.microsoftonline.com/${tenantId}/oauth2/v2.0/authorize`
