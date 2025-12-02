ab3a2222dc85a7f9108c228889f0b550a46fad0f
Kashif <kashif147@gmail.com>
2025-12-02 22:38:46 +0000

    Update Vercel configuration to include rewrites for SPA routing and fix redirect URI in Login component to use dynamic function.

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -168,8 +168,7 @@ const Login = () => {
     localStorage.setItem("pkce_code_verifier", codeVerifier);
     const tenantId = "39866a06-30bc-4a89-80c6-9dd9357dd453";
     const clientId = "ad25f823-e2d3-43e2-bea5-a9e6c9b0dbae";
-    // const redirectUri = getRedirectUri();
-    const redirectUri = "http://localhost:3000"
+    const redirectUri = getRedirectUri();
     const scopes = "openid profile email offline_access";
     const authUrl = new URL(
       `https://login.microsoftonline.com/${tenantId}/oauth2/v2.0/authorize`
